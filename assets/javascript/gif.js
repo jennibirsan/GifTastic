f4fc7f2295e793e92ce336731b5a6f5bc21aa6a1
Jennifer Birsan <jennibirsan@Cornels-MacBook-Pro.local>
2019-05-31 22:29:48 -0500

    js and html done

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,8 +1,12 @@
 
-$("button").on("click", function() {
-  var food = $(this).attr("data-food");
+// setting the offset to render new images everytime a button is pressed
+var offset = 0;
+function displayFoodGifs() {
+  $(".gif").remove();
+  $(".rates").remove();
+  var food = $(this).attr("data-name");
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    food + "&api_key=dlKuW4v13jmJLirr9iKljBKx241oemPR&limit=10";
+    food + "&api_key=dlKuW4v13jmJLirr9iKljBKx241oemPR&limit=10&offset=" + offset;
 // JB API KEY: dlKuW4v13jmJLirr9iKljBKx241oemPR
   $.ajax({
     url: queryURL,
@@ -12,19 +16,64 @@ $("button").on("click", function() {
       var results = response.data;
 
       for (var i = 0; i < results.length; i++) {
-        var gifDiv = $(".card-body");
+        
+        var gifDiv = $("<div class='foodPorn'>");
 
         var rating = results[i].rating;
 
-        var p = $("<p>").text("Rating: " + rating);
+        var p = $("<p class='rates'>").text("Rating: " + rating);
 
         var foodImage = $("<img>");
-        foodImage.attr("src", results[i].images.fixed_height.url);
-
-        gifDiv.prepend(p);
-        gifDiv.prepend(foodImage);
+        var pauseGif = results[i].images.fixed_height_still.url;
+        var playGif = results[i].images.fixed_height.url;
+        foodImage.attr("src", pauseGif);
+        foodImage.attr("data-pause", pauseGif);
+        foodImage.attr("data-play", playGif);
+        foodImage.attr("data-state", "still");
+        foodImage.addClass("gif");
+        gifDiv.append(p);
+        gifDiv.append(foodImage);
 
         $("#gifs-appear-here").prepend(gifDiv);
+        
       }
+      offset = offset+10;
     });
+}
+
+$(document).on("click", ".food-btn", displayFoodGifs);
+
+
+var foods = ["Cake", "Beer", "Pizza", "Tacos", "Ice Cream", "Soda", "Burger"];
+function renderButtons() {
+  $("#buttons-view").empty();
+  for (var i = 0; i < foods.length; i++) {
+    var a = $("<button>");
+    a.addClass("food-btn");
+    a.addClass("btn");
+    a.addClass("btn-outline-info")
+    a.attr("data-name", foods[i]);
+    a.text(foods[i]);
+    $("#buttons-view").append(a);
+  }
+}
+$("#add-food").on("click", function(event) {
+  event.preventDefault();
+  var foodie = $("#food-input").val().trim();
+  foods.push(foodie);
+  renderButtons();
+});
+
+renderButtons();
+
+
+$(document).on("click",".gif", function() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-play"));
+    $(this).attr("data-state", "play");
+  } else {
+    $(this).attr("src", $(this).attr("data-pause"));
+    $(this).attr("data-state", "still");
+  }
 });
